18df69b97d69a097f8b748b10eb008ae8e83f36c
長谷部亮 <wtc.r.h173@gmail.com>
2017-10-14 19:08:47 +0900

    背景画像とフッターを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
   # before_actionで下で定義したメソッドを実行
   before_action :configure_permitted_parameters, if: :devise_controller?
 
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ActiveRecord::Base
   mount_uploader :photo, PhotoUploader
   belongs_to :user
+  validates :photo, presence: true
 end
